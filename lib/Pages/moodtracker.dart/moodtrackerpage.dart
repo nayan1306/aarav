e0ebb5b0d216b3e6eef677e441b558502a3fa9b4
nayan1306 <vnayan1306@gmail.com>
2025-02-20 21:16:24 +0530

    addded mood tracker page

---
+++ lib/Pages/moodtracker.dart/moodtrackerpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoodTrackerPage extends StatefulWidget {
+  const MoodTrackerPage({super.key});
+
+  @override
+  State<MoodTrackerPage> createState() => _MoodTrackerPageState();
+}
+
+class _MoodTrackerPageState extends State<MoodTrackerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
