1cf59f16c295344b53b14b386f73d927520400e2
nayan1306 <vnayan1306@gmail.com>
2025-03-04 16:41:58 +0530

    added duratioins as a parameter in calm page

--- lib/Pages/breathing/breathingpages/energypage.dart
+++ lib/Pages/breathing/breathingpages/energypage.dart
@@ -6,21 +6,56 @@ class EnergyPage extends StatefulWidget {
   const EnergyPage({super.key});
 
   @override
-  State<EnergyPage> createState() => _CalmPageState();
+  State<EnergyPage> createState() => _EnergyPageState();
 }
 
-class _CalmPageState extends State<EnergyPage> {
-  // TODO: Change the title and  image Link
+class _EnergyPageState extends State<EnergyPage> {
   final List<Map<String, dynamic>> breathingExercises = [
-    {"title": "Decompress", "image": "assets/images/sphere.png"},
-    {"title": "Calm", "image": "assets/images/sphere.png"},
-    {"title": "Free", "image": "assets/images/sphere.png"},
-    {"title": "Decompress1", "image": "assets/images/sphere.png"},
-    {"title": "Calm2", "image": "assets/images/sphere.png"},
-    {"title": "Free3", "image": "assets/images/sphere.png"},
-    {"title": "Decompress4", "image": "assets/images/sphere.png"},
-    {"title": "Calm5", "image": "assets/images/sphere.png"},
-    {"title": "Free6", "image": "assets/images/sphere.png"},
+    {
+      "title": "Decompress",
+      "image": "assets/images/sphere.png",
+      "durations": [3, 2, 3, 2],
+    },
+    {
+      "title": "Calm",
+      "image": "assets/images/sphere.png",
+      "durations": [4, 3, 4, 3],
+    },
+    {
+      "title": "Free",
+      "image": "assets/images/sphere.png",
+      "durations": [5, 4, 5, 4],
+    },
+    {
+      "title": "Decompress1",
+      "image": "assets/images/sphere.png",
+      "durations": [3, 2, 3, 2],
+    },
+    {
+      "title": "Calm2",
+      "image": "assets/images/sphere.png",
+      "durations": [4, 3, 4, 3],
+    },
+    {
+      "title": "Free3",
+      "image": "assets/images/sphere.png",
+      "durations": [5, 4, 5, 4],
+    },
+    {
+      "title": "Decompress4",
+      "image": "assets/images/sphere.png",
+      "durations": [3, 2, 3, 2],
+    },
+    {
+      "title": "Calm5",
+      "image": "assets/images/sphere.png",
+      "durations": [4, 3, 4, 3],
+    },
+    {
+      "title": "Free6",
+      "image": "assets/images/sphere.png",
+      "durations": [5, 4, 5, 4],
+    },
   ];
 
   Widget _buildBreathingTileRow(List<Map<String, dynamic>> items) {
@@ -34,14 +69,14 @@ class _CalmPageState extends State<EnergyPage> {
                 assetImagePath: exercise["image"],
                 title: exercise["title"],
                 onTap: () {
-                  print("Tapped: ${exercise["title"]}"); // Debug log
+                  print("Tapped: ${exercise["title"]}");
                   Navigator.of(context).push(
                     MaterialPageRoute(
                       builder:
                           (context) => BreathingLandingPage(
                             assetImagePath: exercise["image"],
                             title: exercise["title"],
-                            durations: [2, 1, 2, 1],
+                            durations: exercise["durations"],
                           ),
                     ),
                   );
